5426bb4976a99b9a7e62e983e506890e2b5e6d15
TomomitsuKeruma <78133385+TomomitsuKeruma@users.noreply.github.com>
2021-04-03 18:19:25 +0900

    投稿検索機能実装完了

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -5,4 +5,12 @@ class Tweet < ApplicationRecord
   has_one_attached :image
   has_many :comments
   has_many_attached :images
+
+  def self.search(search)
+    if search != ""
+      Tweet.where('text LIKE(?)', "%#{search}%")
+    else
+      Tweet.all
+    end
+  end
 end
