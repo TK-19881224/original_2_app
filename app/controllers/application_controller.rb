0027fb96f3ece1933d7bd0839de9a7aef5848720
TomomitsuKeruma <78133385+TomomitsuKeruma@users.noreply.github.com>
2021-04-04 11:49:49 +0900

    applicationcontroller修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :move_to_index, except: [:index, :show, :search]
 
   def search
     @tweets = Tweet.search(params[:keyword])
